bd5297e6f5933dba456cef368e1f9ab98d93d068
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-11-30 19:21:17 +0800

    fix: cleaning up code

--- src/component/EditorComponent.js
+++ src/component/EditorComponent.js
@@ -1,9 +1,18 @@
 class EditorComponent extends HTMLElement {
   connectedCallback() {
-    this.setAttribute('id', 'editor');
     this.classList.add('flex');
     this.classList.add('flex-col');
     this.classList.add('flex-1');
+    this.classList.add('my-1');
+    this.classList.add('py-1');
+
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <div id="editor" class="flex flex-col flex-1 overflow-auto"></div>
+    `;
   }
 }
 
--- src/view/create.js
+++ src/view/create.js
@@ -15,7 +15,7 @@ class Create extends HTMLElement {
     import('../component/IngredientComponent')
       .then(() => {
         const ingredientElement = document.createElement('ingredient-element');
-        this.querySelector('#ingredient-container').appendChild(ingredientElement);
+        this.querySelector('#list-container').appendChild(ingredientElement);
       });
   }
 
@@ -32,25 +32,39 @@ class Create extends HTMLElement {
     }
   }
 
-  disconnectedCallback() {
-    this.querySelector('#ingredient-list-counter')
-      .removeEventListener('click', (e) => this.addNewIngredientElement(e));
+  submitRecipeEventHandler(e, quill) {
+    e.preventDefault();
+
+    const title = this.querySelector('#recipe-name').value;
+    const key = title.toLowerCase()
+      .split(' ')
+      .join('-');
+    const [file] = this.querySelector('#recipe-image-uploader').files;
+    const imgBlob = file ? URL.createObjectURL(file) : null;
+    const ingredientContainer = this.querySelector('#list-container');
+    const ingredientElements = ingredientContainer.querySelectorAll('#ingredient-material');
+    const ingredientArray = Array.from(ingredientElements).map((ingredient) => ingredient.value);
+    const instruction = quill.getContents();
+
+    const newRecipe = {
+      key,
+      title,
+      thumb: imgBlob,
+      ingredients: ingredientArray,
+      instruction: instruction.ops[0].insert,
+    };
+    console.log(newRecipe);
+
+    // cacheRecipeData(newRecipe, {
+    //   storageName: 'userCache',
+    //   category: 'created',
+    // });
+
+    console.log('recipe submitted');
   }
 
-  connectedCallback() {
-    this.classList.add('flex');
-    this.classList.add('flex-col');
-    this.classList.add('pt-16');
-    this.classList.add('min-h-screen');
-    this.render();
-
-    this.querySelector('#ingredient-list-counter')
-      .addEventListener('click', (e) => this.addNewIngredientElement(e));
-
-    this.querySelector('#recipe-image-uploader')
-      .addEventListener('change', () => this.recipeImageSelector(this.querySelector('#recipe-image-uploader'),
-        this.querySelector('#recipe-image-preview')));
-
+  // eslint-disable-next-line class-methods-use-this
+  initQuill() {
     const options = {
       modules: {
         toolbar: [
@@ -62,38 +76,35 @@ class Create extends HTMLElement {
       placeholder: 'Tuliskan langkah-langkah resep disini...',
       theme: 'snow',
     };
-    // eslint-disable-next-line no-unused-vars
+
     const quill = new Quill('#editor', options);
 
-    this.querySelector('#submit-btn').addEventListener('click', (e) => {
-      e.preventDefault();
-      // select all form inputs
-      const recipeTitle = this.querySelector('#recipe-name').value;
-      const recipeId = recipeTitle.toLowerCase().split(' ');
-      const [file] = this.querySelector('#recipe-image-uploader').files;
-      const imgBlob = file ? URL.createObjectURL(file) : null;
-      const ingredientContainer = this.querySelector('#ingredient-container');
-      const ingredientElements = ingredientContainer.querySelectorAll('#ingredient-material');
-      // eslint-disable-next-line max-len
-      const ingredientArray = Array.from(ingredientElements).map((ingredient) => ingredient.value);
-      const recipeInstruction = quill.getContents();
-      // store data into object
-      const recipeObject = {
-        key: recipeId.join('-'),
-        title: recipeTitle,
-        thumb: imgBlob,
-        ingredients: ingredientArray,
-        instruction: recipeInstruction.ops[0].insert,
-      };
-      console.log(recipeObject);
-      // submit to local storage
-      cacheRecipeData(recipeObject, {
-        storageName: 'userCache',
-        category: 'created',
-      });
+    return quill;
+  }
 
-      console.log('recipe submitted');
-    });
+  disconnectedCallback() {
+    this.querySelector('#ingredient-list-counter')
+      .removeEventListener('click', (e) => this.addNewIngredientElement(e));
+  }
+
+  connectedCallback() {
+    this.classList.add('flex');
+    this.classList.add('flex-col');
+    this.classList.add('pt-16');
+    this.classList.add('min-h-screen');
+
+    this.render();
+
+    this.querySelector('#ingredient-list-counter')
+      .addEventListener('click', (e) => this.addNewIngredientElement(e));
+
+    this.querySelector('#recipe-image-uploader')
+      .addEventListener('change', () => this.recipeImageSelector(this.querySelector('#recipe-image-uploader'),
+        this.querySelector('#recipe-image-preview')));
+
+    const quill = this.initQuill();
+
+    this.querySelector('#submit-btn').addEventListener('click', (e) => this.submitRecipeEventHandler(e, quill));
   }
 
   render() {
@@ -102,33 +113,31 @@ class Create extends HTMLElement {
       <section id="form-component" class="flex flex-col flex-1 p-4">
         <h2>Resep Baru</h2>
         <form class="flex flex-col flex-1 h-full border-sm border-gray-400 mt-2">
-          <div class="flex flex-col mb-2">
+          <section id="recipe-title-container" class="flex flex-col mb-2">
             <label for="" class="text-sm text-green-800">Nama resep</label>
             <input id="recipe-name" class="bg-white border-b-2 border-gray-200 focus:outline-none focus:border-green-200 my-1 p-1 text-sm text-gray-800 p-1" type="text" placeholder="tuliskan nama resep disini..."/>
-          </div>
-          <div class="flex pb-1 mb-2">
-            <label for="" class="text-sm text-green-800">Foto Masakan</div>
-          </div>
+          </section>
           <section id="recipe-image-container" class="w-full flex flex-col mb-2">
-            <div class="flex flex-col flex-1 border-2 p-1">
+            <label for="" class="text-sm text-green-800">Foto Masakan</div>
+            <div class="flex flex-col flex-1 border-2 my-1 p-1">
               <input type="file" id="recipe-image-uploader" name="recipe-image" accept="image/png, image/jpeg, image/webp" class="text-sm">
               <img id="recipe-image-preview" src="#" alt="recipe-image-preview" class="text-sm hidden"/>
             </div>
           </section>
-          <div class="flex justify-between pb-1 mb-2">
-            <label for="" class="text-sm text-green-800">Bahan baku</label>
-            <button id="ingredient-list-counter" class="flex items-center text-sm text-blue-500 w-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
-              </svg>
-            </button>
-          </div>
-          <section id="ingredient-container" class="w-full h-32 border-b-2 mb-2 overflow-auto no-scrollbar">
+          <section id="recipe-ingredient-container" class="flex flex-col">
+            <div class="flex justify-between pb-1 mb-2">
+              <label for="" class="text-sm text-green-800">Bahan baku</label>
+              <button id="ingredient-list-counter" class="flex items-center text-sm text-blue-500 w-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+                </svg>
+              </button>
+            </div>          
           </section>
-          <div class="flex justify-between pb-1 mb-2">
-            <label for="" class="text-sm text-green-800">Instruksi Memasak</label>
+          <div id="list-container" class="w-full h-32 border-b-2 mb-2 overflow-auto no-scrollbar">
           </div>
-          <section id="step-container" class="flex flex-col flex-1">
+          <section id="recipe-step-container" class="flex flex-col flex-1">
+            <label for="" class="text-sm text-green-800">Instruksi Memasak</label>
             <editor-element></editor-element> 
           </section>
           <div class="flex justify-end pt-2">
